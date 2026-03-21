1406cf0da2df97f2099c5248e395e3bc3627fc38
Barry-Michael van Wyk <bmvanwyk@gmail.com>
2026-03-21 07:26:15 +0200

    cleanup: remove unused imports and legacy functions in TulipReport.kt

--- tulip-runtime/src/main/kotlin/io/github/tulipltt/tulip/report/TulipReport.kt
+++ tulip-runtime/src/main/kotlin/io/github/tulipltt/tulip/report/TulipReport.kt
@@ -1,14 +1,7 @@
 package io.github.tulipltt.tulip.report
 
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
 import java.io.File
-import java.io.IOException
-import java.nio.charset.StandardCharsets
-import java.util.*
-import java.util.zip.GZIPInputStream
 import kotlinx.serialization.json.Json
-import org.python.util.PythonInterpreter
 
 fun createHtmlReport(outputFilename: String) {
     val jsonFile = File(outputFilename)
@@ -25,20 +18,4 @@ fun createHtmlReport(outputFilename: String) {
     
     println("Generating HTML report: $htmlOutputPath")
     TulipReportGenerator.createReport(outputFilename, htmlOutputPath)
-}
-
-@Throws(IOException::class)
-fun decompress(compressedData: ByteArray): String? {
-    val bis = ByteArrayInputStream(compressedData)
-    val gzip = GZIPInputStream(bis)
-    val bos = ByteArrayOutputStream()
-    val buffer = ByteArray(1024)
-    var len: Int
-    while ((gzip.read(buffer).also { len = it }) != -1) {
-        bos.write(buffer, 0, len)
-    }
-    gzip.close()
-    bis.close()
-    bos.close()
-    return bos.toString(StandardCharsets.UTF_8.name())
 }
